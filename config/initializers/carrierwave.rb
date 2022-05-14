12e2e93b50434cc209f3cd3fae5cee3390255d04
Wiechsa <98656443+Wiechsaa@users.noreply.github.com>
2022-05-14 17:15:28 +0200

    Nie dla psa

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAXXLXWLKYYREF6UER',
-    aws_secret_access_key: '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',
+    aws_access_key_id: '#',
+    aws_secret_access_key: '#',
     use_iam_profile: false,
     region: 'eu-central-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'http://onlybunnies.s3-website.eu-central-1.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'onlybunnies'
+  config.fog_directory  = '#'
   config.fog_public     = false
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   # For an application which utilizes multiple servers but does not need caches persisted across requests,
